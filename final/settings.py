d199bfe784008440563faea305866eb36ecf833d
terran5992 <56596358+terran5992@users.noreply.github.com>
2020-02-15 19:35:32 +0800

    updated requirements

--- final/settings.py
+++ final/settings.py
@@ -123,9 +123,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/static/',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
