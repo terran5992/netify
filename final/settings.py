97713d3a9679ef80f917ef03e0fa3650f3474244
terran5992 <56596358+terran5992@users.noreply.github.com>
2020-02-15 19:38:03 +0800

    updated requirements

--- final/settings.py
+++ final/settings.py
@@ -123,12 +123,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/static/',
+    # os.path.join(BASE_DIR, "static"),
+    # '/static/',
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
